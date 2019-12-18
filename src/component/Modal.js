129d4cc810fea856df76574b7a114a58d41ca3d5
Sahil Singh <sahilsinghmca@gmail.com>
2019-12-18 22:36:07 +0530

    finding hp strength and weakness

--- src/component/Modal.js
+++ src/component/Modal.js
@@ -16,7 +16,7 @@ class Modal extends Component {
     this.setState({ filteredPokemonCards: this.props.pokemonCards })
   }
 
-  cuteImoji = () => {
+  cuteImoji = (happiness) => {
     for (let i = 0; i < 5; i++) {
       return (
         <img src={cuteImg} alt="cuteImage" />
@@ -47,32 +47,43 @@ class Modal extends Component {
         <div className="modal-container">
           <SearchBar onChange={text => this.onSearch(text)} />
           <div className="pokemon-modal-list">
-            {filteredPokemonCards.map((card, index) => (
-              <div key={index} className="pokemon-modal-card">
-                <div className="add-Icon" onClick={() => this.props.addCard(card)}>ADD</div>
-                <div className="pokemon-img">
-                  <img src={card.imageUrl} alt={card.name} />
+            {filteredPokemonCards.map((card, index) => {
+              const hp = card.hp > 100 ? 100 : card.hp
+              const strength = card.attacks ? ((card.attacks.length * 50) > 100 ? 100 : (card.attacks.length * 50)) : 0
+              const weakness = card.weaknesses ? ((card.weaknesses.length * 100) > 100 ? 100 : (card.weaknesses.length * 100)) : 0
+              let damage = 0
+              const d = card.attacks ? card.attacks.map(c => {
+                if (c.damage !== undefined) damage = damage + Number(c.damage.replace(/[^0-9]/g,''))
+              }) : 0
+              const happiness = ((hp / 10) + (damage / 10) + 10 - (weakness)) / 5
+
+              return (
+                <div key={index} className="pokemon-modal-card">
+                  <div className="add-Icon" onClick={() => this.props.addCard(card)}>ADD</div>
+                  <div className="pokemon-img">
+                    <img src={card.imageUrl} alt={card.name} />
+                  </div>
+                  <div className="pokemon-details">
+                    <h3>{card.name}</h3>
+                    <div className="tube">
+                      <div className="level-name">HP</div>
+                      <div className="level-tube"><div className="level" style={{ width: `${hp}%` }} /></div>
+                    </div>
+                    <div className="tube">
+                      <div className="level-name">STR</div>
+                      <div className="level-tube"><div className="level" style={{ width: `${strength}%` }} /></div>
+                    </div>
+                    <div className="tube">
+                      <div className="level-name">WEAK</div>
+                      <div className="level-tube"><div className="level" style={{ width: `${weakness}%` }} /></div>
+                    </div>
+                    <div className="cute-score">
+                      {this.cuteImoji(happiness)}
+                    </div>
+                  </div>
                 </div>
-                <div className="pokemon-details">
-                  <h3>{card.name}</h3>
-                  <div className="tube">
-                    <div className="level-name">HP</div>
-                    <div className="level-tube"><div className="level" /></div>
-                  </div>
-                  <div className="tube">
-                    <div className="level-name">STR</div>
-                    <div className="level-tube"><div className="level" /></div>
-                  </div>
-                  <div className="tube">
-                    <div className="level-name">WEAK</div>
-                    <div className="level-tube"><div className="level" /></div>
-                  </div>
-                  <div className="cute-score">
-                    {this.cuteImoji()}
-                  </div>
-                </div>
-              </div>
-            ))}
+              )
+            })}
           </div>
         </div>
       </div>
